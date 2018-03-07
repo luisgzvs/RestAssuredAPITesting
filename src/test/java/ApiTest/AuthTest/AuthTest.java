bb97d4e981c5f2e130d946216fd1e86520959bc4
Luis Gonzalez <luisgzvs@gmail.com>
2018-03-07 10:04:53 -0600

    adding authentication test

--- src/test/java/ApiTest/AuthTest/AuthTest.java
+++ src/test/java/ApiTest/AuthTest/AuthTest.java
@@ -1,19 +1,30 @@
 package ApiTest.AuthTest;
 
+import Entities.AuthUser;
+import Entities.User;
 import org.testng.annotations.*;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class AuthTest {
 
+    //@Test
+    //@Parameters({"AuthTokenEndPoint"})
+    public String GetAuthenticationToken(String AuthTokenEndPoint) {
+        AuthUser user = new AuthUser();
+        user.setUsername("luisgzvs");
+        user.setPassword("12345");
+        String response;
+        Response resp =
+                given().header("Authorization", "Bearer "+ AuthTokenEndPoint)
+                .post(AuthTokenEndPoint, user);
+        return response = resp.toString();
+    }
+
     @Test
     @Parameters({"AuthTokenEndPoint"})
-    public void GetAuthenticationToken(String token) {
-
-        Response resp =
-                given().header("Authorization", "Bearer "+token)
-                        .body("html.body", equalTo("works=true"));
+    public void printResponse(String AuthTokenEndPoint){
+        System.out.print(GetAuthenticationToken(AuthTokenEndPoint));
     }
 
     }

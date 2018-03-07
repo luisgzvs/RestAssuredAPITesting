f2ccae4f67d1830230a89f6985f486bdeaf1398d
Luis Gonzalez <luis.gonzalez@avantica.net>
2018-03-07 11:37:23 -0600

    succesfully getting token for auth

--- src/test/java/ApiTest/AuthTest/AuthTest.java
+++ src/test/java/ApiTest/AuthTest/AuthTest.java
@@ -1,30 +1,29 @@
 package ApiTest.AuthTest;
 
-import Entities.AuthUser;
-import Entities.User;
-import org.testng.annotations.*;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
 
 public class AuthTest {
 
-    //@Test
-    //@Parameters({"AuthTokenEndPoint"})
-    public String GetAuthenticationToken(String AuthTokenEndPoint) {
-        AuthUser user = new AuthUser();
-        user.setUsername("luisgzvs");
-        user.setPassword("12345");
-        String response;
+    private String token;
+
+    private String GetAuthenticationToken(String AuthTokenEndPoint) {
+
         Response resp =
-                given().header("Authorization", "Bearer "+ AuthTokenEndPoint)
-                .post(AuthTokenEndPoint, user);
-        return response = resp.toString();
+                given().contentType("application/json")
+                        .post(AuthTokenEndPoint);
+        JsonPath jsonPathEvaluator = resp.jsonPath();
+        return token = jsonPathEvaluator.get("token");
     }
 
     @Test
     @Parameters({"AuthTokenEndPoint"})
-    public void printResponse(String AuthTokenEndPoint){
-        System.out.print(GetAuthenticationToken(AuthTokenEndPoint));
+    public void printResponse(String AuthTokenEndPoint) {
+        System.out.println(GetAuthenticationToken(AuthTokenEndPoint));
     }
 
-    }
+}
